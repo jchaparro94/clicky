1f6c8ac35b031bc858bc7552d80e358710161d5e
Jose Chaparro <josechaparro@Joses-MacBook-Pro.local>
2019-03-30 11:46:11 -0700

    Updates

---
+++ precache-manifest.1b252972244a9a6b0862c34ba976decd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3821ceef7b555b0d9b6b",
+    "url": "/clicky/static/css/main.74432723.chunk.css"
+  },
+  {
+    "revision": "3821ceef7b555b0d9b6b",
+    "url": "/clicky/static/js/main.2b4ada9a.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  },
+  {
+    "revision": "50f83c45e05acae0f08b",
+    "url": "/clicky/static/css/2.350be9e7.chunk.css"
+  },
+  {
+    "revision": "50f83c45e05acae0f08b",
+    "url": "/clicky/static/js/2.d56fcf06.chunk.js"
+  },
+  {
+    "revision": "f6c91fb63b307236c33c1727adb9dd7f",
+    "url": "/clicky/index.html"
+  }
+];
